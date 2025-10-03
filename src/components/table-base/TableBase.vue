16460196aa1e7ca27a7a7b6be434fb61599f6b0f
Makszi Péter <peter.makszi@aventail.hu>
2025-10-03 11:50:56 +0200

    lábléc félkövérítés

--- src/components/table-base/TableBase.vue
+++ src/components/table-base/TableBase.vue
@@ -67,6 +67,24 @@
           </td>
         </tr>
       </template>
+      <template v-if="footerRowItems !== null">
+        <tr>
+          <template v-for="field in fields">
+            <td>
+              <slot
+                  :index="field.key"
+                  name="td"
+                  :field="field.key"
+                  :row="footerRowItems"
+                  :item="field.key in footerRowItems ? footerRowItems[field.key] : null"
+                  :value="field.key in footerRowItems ? footerRowItems[field.key] : null"
+              >
+                <b>{{ field.key in footerRowItems ? footerRowItems[field.key] : '' }}</b>
+              </slot>
+            </td>
+          </template>
+        </tr>
+      </template>
     </tbody>
   </table>
 </template>
@@ -129,6 +147,13 @@ export default {
      */
     tdClass: {
       type: String,
+      default: null,
+    },
+    /**
+     * the footer row content
+     */
+    footerRowItems: {
+      type: Object,
       default: null,
     },
   },
